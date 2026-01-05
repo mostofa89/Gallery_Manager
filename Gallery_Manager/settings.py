da9de0b1ca6d821125c7664144b1925b9be87d96
mostofa89 <mostofakamaljoy89@gmail.com>
2026-01-05 18:41:49 +0600

    Fix Render deployment configuration

--- Gallery_Manager/settings.py
+++ Gallery_Manager/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# Only include static dir if it exists
+STATICFILES_DIRS = [BASE_DIR / 'static'] if (BASE_DIR / 'static').exists() else []
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Whitenoise configuration
